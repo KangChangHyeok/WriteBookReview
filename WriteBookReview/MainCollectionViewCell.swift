c180f31aa756a7d2d0fd8c16fcca1b36ec5b3c06
KangChangHyeok <dpkyle97@gmail.com>
2022-08-01 22:49:15 +0900

    UI 작업
    -메인뷰 버튼,컬렉션뷰 생성후 snapkit으로 autoLayout작업 완료.

---
+++ WriteBookReview/MainCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MainCollectionViewCell.swift
+//  WriteBookReview
+//
+//  Created by 강창혁 on 2022/08/01.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
--- WriteBookReview/ViewController.swift
+++ WriteBookReview/ViewController.swift
@@ -18,13 +18,17 @@ struct VCPreView:PreviewProvider {
 
 class MainViewController: UIViewController {
     
-//    let collectionView = UICollectionView()
-    let searchButton = UIButton().then {
+    private let mainColletionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+        $0.backgroundColor = .gray
+        $0.register(MainCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+    }
+    private let searchButton = UIButton().then {
         $0.setTitle("내가 읽은 책 찾기", for: .normal)
         $0.tintColor = .black
         $0.titleLabel?.font = .systemFont(ofSize: 20)
         $0.backgroundColor = .black
     }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpValue()
@@ -32,6 +36,9 @@ class MainViewController: UIViewController {
         setUpConstraints()
     }
     func setUpValue() {
+        //뷰 배경화면 색 설정
+        view.backgroundColor = .systemBackground
+        
         //navigation Controller 에러 해결 코드
         let appearance = UINavigationBarAppearance()
         appearance.configureWithTransparentBackground()
@@ -39,17 +46,27 @@ class MainViewController: UIViewController {
         appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
         navigationItem.standardAppearance = appearance
         navigationItem.scrollEdgeAppearance = appearance
-        //뷰 배경화면 색 설정
-        view.backgroundColor = .systemBackground
+        
+        //navigation bar button item 추가하기
+        let rightNavigationItem = UIBarButtonItem(title: "right", style: .plain, target: self, action: #selector(touched))
+        navigationItem.rightBarButtonItem = rightNavigationItem
+        //collectionView
+        mainColletionView.delegate = self
+        mainColletionView.dataSource = self
+        
+        
     }
     func setUpView() {
         view.addSubview(searchButton)
+        view.addSubview(mainColletionView)
     }
     func setUpConstraints() {
         //컬렉션뷰 레이아웃 설정
-//        self.collectionView.snp.makeConstraints {
-//            $0.top.bottom.leading.trailing.equalToSuperview()
-//        }
+        self.mainColletionView.snp.makeConstraints {
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalTo(searchButton.snp.top)
+        }
         //하단 검색하기 버튼 레이아웃 설정
         
         self.searchButton.snp.makeConstraints {
@@ -59,12 +76,29 @@ class MainViewController: UIViewController {
         }
     }
     
+    @objc func touched() {
+        print("touch")
+    }
+    
 }
 
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! MainCollectionViewCell
+        cell.backgroundColor = .white
+        return cell
+    }
+    
+    
+}
 
-
-
-
+extension MainViewController: UICollectionViewDelegate {
+    
+}
 
 
 
